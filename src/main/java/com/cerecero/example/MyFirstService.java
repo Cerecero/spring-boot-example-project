b97c6ad82a4ef202809bf325be7ee926e8ba3997
Cerecero <ale.rey.cer@outlook.com>
2024-08-01 14:02:04 -0600

    Added dependencie injection

---
+++ src/main/java/com/cerecero/example/MyFirstService.java
@@ -0,0 +1,18 @@
+package com.cerecero.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyFirstService {
+
+    private MyFirstClass myFirstClass;
+    @Autowired
+    public  void  injectDependencies(@Qualifier("mySecondClass") MyFirstClass myFirstClass){
+        this.myFirstClass = myFirstClass;
+    }
+    public String tellAStory(){
+        return  "The dependency is saying : " + myFirstClass.sayHello();
+    }
+}
